81aa05bd36dd5b9200979416b6706df5bcbc4b11
kyomukyomupurin <murakami1728@gmail.com>
2020-08-28 23:22:49 +0900

    :tada: Initial commit

---
+++ convert.py
@@ -0,0 +1,21 @@
+import base64
+import sys
+
+cpp_code = ""
+
+target_file = sys.argv[1]
+
+with open(target_file, "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        cpp_code += line
+
+python_code = "import subprocess\nimport sys\n\ncode = r\"\"\"" + cpp_code + \
+    "\"\"\"\n\nwith open(\'sol.cpp\', \'w\') as f:\n    f.write(code)\n\n" + \
+    "subprocess.Popen([\'g++\', \'-std=c++17\', \'-O2\', \'sol.cpp\']).communicate()\n" + \
+    "subprocess.Popen([\'./a.out\'], stdin=sys.stdin, stdout=sys.stdout).communicate()"
+
+encoded_code = base64.b64encode(python_code.encode())
+
+with open("./sol.py", "w") as f:
+    f.write("import base64\n\nexec(base64.b64decode(b\'" + str(encoded_code, encoding='utf-8') + "\'))")
