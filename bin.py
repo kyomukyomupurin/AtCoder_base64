05b1e8225d2a7a05eed06f3fa6107b9a09988625
kyomukyomupurin <murakami1728@gmail.com>
2021-01-28 22:56:32 +0900

    :recycle: Refactor code

--- bin.py
+++ bin.py
@@ -14,7 +14,7 @@ args = parser.parse_args()
 src = args.cpp
 subprocess.run(["g++-10", "-std=c++20", "-O2", src, "-o", Path(src).stem])
 
-ascii_bin = str(base64.b85encode(zlib.compress(open(Path(src).stem, "rb").read())), encoding="utf-8")
+ascii_bin = str(base64.b85encode(zlib.compress(Path(Path(src).stem).open("rb").read())), encoding="utf-8")
 
 with Path(Path(src).stem + ".py").open("w") as f:
     f.write(Path("src1.txt").open("r").read())
