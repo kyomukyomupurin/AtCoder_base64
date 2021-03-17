be04bdf64f3a7cc50c89ed2622a3bde839b65327
kyomukyomupurin <murakami1728@gmail.com>
2021-03-17 19:27:09 +0900

    :recycle: Refactor code

--- bin.py
+++ bin.py
@@ -14,15 +14,15 @@ args = parser.parse_args()
 src = args.cpp
 subprocess.run(["g++-10", "-std=c++20", "-O2", src, "-o", Path(src).stem])
 
-ascii_bin = str(base64.b85encode(zlib.compress(Path(Path(src).stem).open("rb").read())), encoding="utf-8")
+ascii_bin = str(base64.b85encode(zlib.compress(Path(Path(src).stem).read_bytes())), encoding="utf-8")
 
 with Path(Path(src).stem + ".py").open("w") as f:
-    f.write(Path("src1.txt").open("r").read())
-    f.write("ascii_bin = \"" + ascii_bin + "\"\n")
-    f.write(Path("src2.txt").open("r").read())
+    f.write(Path("src1.txt").read_text())
+    f.write(f"ascii_bin = \"{ascii_bin}\"\n")
+    f.write(Path("src2.txt").read_text())
 
     if args.original:
-        f.write("\n\n# Original source code : \n\"\"\"\n" + Path(src).open("r").read() + "\n\"\"\"")
+        f.write(f"\n\n# Original source code : \n\"\"\"\n{Path(src).read_text()}\n\"\"\"")
 
 sz = sys.getsizeof(ascii_bin)
 print("The size of compressed and encoded binary is {:.1f} KB, {:.1f} % of limit.".format(sz / 1000, sz / 5120))
